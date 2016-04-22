879499477ea29c0d7521ba8af8b00125df0ce854
sphuff <stephen.huffnagle@gmail.com>
2016-04-22 13:16:31 -0400

    Send private message added. Now will append private messages to the user object, and return the message ObjectID

--- API/app.js
+++ API/app.js
@@ -1,6 +1,7 @@
 var express        =        require("express");
 var bodyParser     =        require("body-parser");
 var app            =        express();
+var ObjectID = require('mongodb').ObjectID;
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -142,18 +143,28 @@ app.get('/friendsFor:username', function(req, res){
 });
 
 app.post('/sendPrivateMessage', function(req, res){
-    MongoClient.connect('mongodb://127.0.0.1:27017/Pollr', function(err, db){
+      MongoClient.connect('mongodb://127.0.0.1:27017/Pollr', function(err, db){
       console.dir("Connected");
       if(err) {
         throw err;
       }
-      var collection = db.collection('privateMessages');
-      collection.insert({"createdBy" : req.body.createdBy, "dateCreated" : req.body.dateCreated, "text" : req.body.text, "sentTo" : req.body.sentTo}, function (err, results){
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(results));
-        db.close();
-      });
-    });
+      var collection = db.collection('users');
+      var sendTo = req.body.sentTo; 
+      var messageID = ObjectID();
+      for(i = 0; i < sendTo.length; i++){
+          collection.update(
+            { "username" : sendTo[i]},
+            { $push: {messages: {"id" : messageID, "createdBy" : req.body.createdBy,
+              "dateCreated" : req.body.dateCreated, "text" : req.body.text,
+              "responseNum" : 0}}}
+          );
+          if(i == sendTo.length - 1){
+              res.setHeader('Content-Type', 'application/json');
+              res.send(JSON.stringify(messageID));
+              db.close();
+          }
+      }
+  });
 });
 
 app.post('/sendPublicMessage', function(req, res){
